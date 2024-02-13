772301977d6a8b195342ea011a2ba00c915b2a73
cmpallavi <cmpallavi@gmail.com>
2024-02-12 23:13:54 -0800

    part 1

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -3,6 +3,24 @@
  // Store our API endpoint as queryUrl.
 let url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
 
+function chooseColor(depth){
+  if (depth < 10) return "#00FF00";
+  else if (depth < 30) return "greenyellow";
+  else if (depth < 50) return "yellow";
+  else if (depth < 70) return "orange";
+  else if (depth < 90) return "orangered";
+  else return "#FF0000";
+}
+function colorFunction(feature, layer) {
+  return {
+    color : "black",
+    fillColor : chooseColor(feature.geometry.coordinates[2]),
+    fillOpacity : .7,
+    weight : 0.5
+  };
+}
+
+
 // Perform a GET request to the query URL/
 d3.json(url).then(function (data) {
   // Once we get a response, send the data.features object to the createFeatures function.
@@ -14,18 +32,17 @@ function createFeatures(earthquakeData) {
   // Define a function that we want to run once for each feature in the features array.
   // Give each feature a popup that describes the place and time of the earthquake.
   function featurefunction(feature, layer) {
-    layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
+    layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p><p>Depth = ${feature.geometry.coordinates[2]}</p>`);
   }
 
   // Create a GeoJSON layer that contains the features array on the earthquakeData object.
   // Run the onEachFeature function once for each piece of data in the array.
   let earthquakesLayer = L.geoJSON(earthquakeData, {
+    style: colorFunction,
     onEachFeature: featurefunction,
     pointToLayer: (feature, latlng) => {
-        
           return new L.Circle(latlng, feature.properties.mag*20000);
-        
-      },
+        },
   });
 
   // Send our earthquakes layer to the createMap function/
@@ -70,4 +87,21 @@ function createMap(earthquakesLayer) {
     collapsed: false
   }).addTo(myMap);
 
+  var legend = L.control({position: "bottomright"});
+  legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend"),
+    depth = [-10, 10, 30, 50, 70, 90];
+
+    
+    let innerHTML = "<table style='text-align: center;background: white;padding: 10px'>"
+
+    for (var i = 0; i < depth.length; i++) {
+      innerHTML +=
+      '<tr><td style="background:' + chooseColor(depth[i] + 1) + '">&nbsp;&nbsp;&nbsp;&nbsp;</td> ' + '<td>'+ depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '</td></tr>' : '+</td></tr>');
+    }
+    innerHTML += "</table>"
+    div.innerHTML += innerHTML;
+    return div;
+  };
+  legend.addTo(myMap);
 }
